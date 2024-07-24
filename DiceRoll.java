67dd9acf7b5208cbf4e72dd764cc554c79f973d3
suragsp <surag.patel@cdicollege.ca>
2024-07-24 09:44:55 -0400

    Demo Commit 3

--- DiceRoll.java
+++ DiceRoll.java
@@ -3,20 +3,15 @@ public class DiceRoll {
         int roll;
         int[] diceRolls = new int[20];
         int totalRolls = 0;
-
-        // Sample the rolls
         for (int count = 0; count < 1000; count++) {
             roll = (int)(Math.random() * 20) + 1;
             diceRolls[roll - 1] += 1;
         }
-
-        // Run a report
         for (int index = 0; index < diceRolls.length; index++) {
             System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
             totalRolls += diceRolls[index];
         }
-
         System.out.println("Total rolls were: " + totalRolls);
-        System.out.println("Program by Your Name");
+        System.out.println("Program by Surag Patel");
     }
 }
